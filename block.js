293af1760d0917973591c8193db5dfc741b57c78
Akchansh <73519090+akchansh@users.noreply.github.com>
2020-12-14 07:54:25 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,19 @@
+class Block  {
+    constructor(x, y, width,height){
+      var options = {
+        isStatic : false,
+      }
+      this.width = width
+      this.height = height
+      this.body = Bodies.rectangle(x,y,this.width,this.height,options);
+      World.add(world,this.body)
+    }
+  display(){
+    var pos =this.body.position;
+    push()
+    rectMode(CENTER)
+    rect(pos.x,pos.y,this.width,this.height)
+    pop()
+  }
+  };
+  
